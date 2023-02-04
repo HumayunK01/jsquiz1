08340031bae51c7d188f0f4db562d2d315b5e35e
Humayun Khan <humayunk.pvt@gmail.com>
2023-02-05 00:50:31 +0530

    init

---
+++ script.js
@@ -0,0 +1,91 @@
+// Question No 1:
+console.log("Question 1:");
+var str1 = "Today is";
+var str2 = "a beautiful day";
+var str3 = "In Hawaii.";
+result = str1 + " " + str2 + " " + str3;
+console.log(result);
+
+// Question No 2:
+console.log("Question 2:");
+var enteredAlphabet;
+enteredAlphabet = "d";
+console.log("Entered Alphabet is " + enteredAlphabet);
+switch (enteredAlphabet) {
+  case "a":
+  case "e":
+  case "i":
+  case "o":
+  case "u":
+  case "A":
+  case "E":
+  case "I":
+  case "O":
+  case "U":
+    console.log(enteredAlphabet + " is a vowel");
+    break;
+  default:
+    console.log(enteredAlphabet + " is not a vowel");
+}
+
+// Question No 3:
+console.log("Question 3:");
+var no1;
+var no2;
+var result;
+var operator;
+no1 = prompt("Enter First Number: ");
+no2 = prompt("Enter Second Number: ");
+operator = prompt("Enter operator(+, -, *, /): ");
+console.log("First Number: " + no1);
+console.log("First Number: " + no2);
+switch (operator) {
+  case "+":
+    result = no1 + no2;
+    console.log(result);
+    break;
+  case "-":
+    result = no1 - no2;
+    console.log(result);
+    break;
+  case "*":
+    result = no1 * no2;
+    console.log(result);
+    break;
+  case "/":
+    result = no1 / no2;
+    console.log(result);
+    break;
+  default:
+    console.log("Invalid operator");
+    break;
+}
+
+// Question 4:
+console.log("Question 4:");
+var x = 5,
+  y = 4,
+  z = 5;
+console.log("x = " + x);
+console.log("y = " + y);
+console.log("z = " + z);
+if (x == y && y == z) console.log("Equilateral Triangle");
+else if (x == y || y == z || z == x) console.log("Isosceles Triangle");
+else console.log("Scalene Triangle");
+
+// Question 5:
+// Write a JavaScript program to input electricity unit charge and calculate the total electricity bill according to the given condition:
+// For first 50 units Rs. 0.50/unit
+// For next 100 units Rs. 0.75/unit
+// For next 100 units Rs. 1.20/unit
+// For unit above 250 Rs. 1.50/unit. An additional surcharge of 20% is added to the bill.
+// For example, input: 20 units then bill is Rs10
+// Total Units: 300
+// 50 * 0.5 = 25
+// 100 * 0.75 = 75
+// 100 * 1.2 = 120
+// 50 * 1.5 = 75
+// Total = 295 + 20% surcharge on 295
+// Result = INR 354
+
+console.log("Q5 END =====================");
