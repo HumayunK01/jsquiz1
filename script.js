31fa8e00c44e702d988e1239c94d137e06b1269e
Humayun Khan <humayunk.pvt@gmail.com>
2023-02-07 00:54:48 +0530

    init

--- script.js
+++ script.js
@@ -32,13 +32,10 @@ switch (true) {
 
 // Question No 3:
 console.log("Question 3:");
-var no1;
-var no2;
+var no1 = 45;
+var no2 = 67;
+var operator = "*";
 var result;
-var operator;
-no1 = prompt("Enter First Number: ");
-no2 = prompt("Enter Second Number: ");
-operator = prompt("Enter operator(+, -, *, /): ");
 console.log("First Number: " + no1);
 console.log("Second Number: " + no2);
 switch (operator) {
@@ -76,18 +73,19 @@ else if (x == y || y == z || z == x) console.log("Isosceles Triangle");
 else console.log("Scalene Triangle");
 
 // Question 5:
-// Write a JavaScript program to input electricity unit charge and calculate the total electricity bill according to the given condition:
-// For first 50 units Rs. 0.50/unit
-// For next 100 units Rs. 0.75/unit
-// For next 100 units Rs. 1.20/unit
-// For unit above 250 Rs. 1.50/unit. An additional surcharge of 20% is added to the bill.
-// For example, input: 20 units then bill is Rs10
-// Total Units: 300
-// 50 * 0.5 = 25
-// 100 * 0.75 = 75
-// 100 * 1.2 = 120
-// 50 * 1.5 = 75
-// Total = 295 + 20% surcharge on 295
-// Result = INR 354
+console.log("Question 5:");
+var unit = 130;
+var total = 0;
 
-console.log("Q5 END =====================");
+if (unit > 250) {
+  total = (unit - 250) * 1.5 + 100 * 1.2 + 100 * 0.75 + 50 * 0.5;
+  total = total + total * 0.2;
+} else if (unit > 150) {
+  total = unit - 150 * 1.2 + 100 * 0.75 + 50 * 0.5;
+} else if (unit > 50) {
+  total = unit - 50 * 0.75 + 50 * 0.5;
+} else {
+  total = unit * 0.5;
+}
+
+console.log("Result = INR " + total);
